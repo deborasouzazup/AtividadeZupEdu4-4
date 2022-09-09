b59724ac21d6c637d10ecf63e42d8b481898a426
Debora Rodrigues <deboral.p@hotmail.com>
2022-09-09 14:57:29 -0300

    Finalizar logica de alert para aplicacao

--- LearningTask-4.4/FormViewController.swift
+++ LearningTask-4.4/FormViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class FormViewController: UIViewController {
-
+    typealias MensagemDeValidacao = String
+    
     @IBOutlet weak var nomeTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var areaAtuacaoTextField: UITextField!
@@ -17,43 +18,61 @@ class FormViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func botaoSubmissaoPressionado(_ sender: UIButton) {
-        exibeAlertaDeRevisao()
+        switch informacaoValida() {
+        case (false, let mensagem):
+            exibeAlerta(para: mensagem)
+        default:
+            exibeAlertaDeConfirmacao()
+        }
     }
     
-    func exibeAlertaDeRevisao() {
-        let mensagem = """
-            Antes de enviarmos, por favor, revise seus dados:
+    func emailValido(_ email: String) -> Bool {
+        let regexPattern = #"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,64}"#
         
-            Nome: \(nomeTextField.text!)
-            Email: \(emailTextField.text!)
-            Área de Atuação: \(areaAtuacaoTextField.text!)
-            Status Profissional: \(statusProfissionalTextField.text!)
-        """
-        
-        let alert = UIAlertController(title: "Quase lá!", message: mensagem, preferredStyle: .actionSheet)
-        
-        alert.addAction(UIAlertAction(title: "Confirmar", style: .default, handler: acaoDeConfirmacaoDisparada))
-        
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
-        self.present(alert, animated: true, completion: nil)
+        return NSPredicate(format: "SELF MATCHES %@", regexPattern).evaluate(with: email)
     }
     
-    func acaoDeConfirmacaoDisparada(_ action: UIAlertAction) {
-        // executa alguma lógica aqui e ...
-        exibeAlertaDeConfirmacao()
+    func informacaoValida() -> (Bool, MensagemDeValidacao?) {
+        if let nome = nomeTextField.text, nome.isEmpty {
+            return (false, "Nome não pode estar em branco.")
+        }
+        
+        guard let email = emailTextField.text,
+              !email.isEmpty else{
+            return (false,"Email não pode estar em branco.")
+        }
+        
+        guard emailValido(email) else {
+            return (false, "O email informado é inválido.")
+        }
+        
+        if let areaDeAtuacao = areaAtuacaoTextField.text, areaDeAtuacao.isEmpty {
+            return (false,"Informe sua área de atuação.")
+        }
+        
+        if let statusProfissional = statusProfissionalTextField.text, statusProfissional.isEmpty {
+            return (false,"Informe seu status profissional.")
+        }
+        
+        return (true, nil)
     }
+    
+    func exibeAlerta(para mensagemDeValidacao: MensagemDeValidacao?) {
+        let mensagem = mensagemDeValidacao ?? " Verifique os dados informados e tente novamente."
+        
+        let alert = UIAlertController(title: "Quase lá", message: mensagem, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
     
     func exibeAlertaDeConfirmacao() {
         let alert = UIAlertController(title: "Feito!", message: "Verifique seu email e tenha acesso ao documento.", preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "Ok!", style: .cancel, handler: nil))
-        
         self.present(alert, animated: true, completion: nil)
     }
-
-
 }
 
